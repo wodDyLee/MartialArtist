505026802dceabdb3a90b40f5d0c0969e7a10db2
iMranRan <70613492+wodDyLee@users.noreply.github.com>
2021-10-04 08:49:08 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,77 @@
+# -*- coding: UTF-8 -*-
+import requests as req
+import json,sys,time
+import random
+#先注册azure应用,确保应用有以下权限:
+#files:	Files.Read.All、Files.ReadWrite.All、Sites.Read.All、Sites.ReadWrite.All
+#user:	User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
+#mail:  Mail.Read、Mail.ReadWrite、MailboxSettings.Read、MailboxSettings.ReadWrite
+#注册后一定要再点代表xxx授予管理员同意,否则outlook api无法调用
+
+
+
+
+
+
+path=sys.path[0]+r'/1.txt'
+num1 = 0
+
+api_list = [
+    r'https://graph.microsoft.com/v1.0/me/drive/root',
+    r'https://graph.microsoft.com/v1.0/me/drive',
+    r'https://graph.microsoft.com/v1.0/drive/root',
+    r'https://graph.microsoft.com/v1.0/users',
+    r'https://graph.microsoft.com/v1.0/me/messages',
+    r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
+    r'https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta',
+    r'https://graph.microsoft.com/v1.0/me/drive/root/children',
+    r'https://graph.microsoft.com/v1.0/me/mailFolders',
+    r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories'
+]
+
+def gettoken(refresh_token):
+    headers={'Content-Type':'application/x-www-form-urlencoded'
+            }
+    data={'grant_type': 'refresh_token',
+          'refresh_token': refresh_token,
+          'client_id':id,
+          'client_secret':secret,
+          'redirect_uri':'http://localhost:53682/'
+         }
+    html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
+    jsontxt = json.loads(html.text)
+    refresh_token = jsontxt['refresh_token']
+    access_token = jsontxt['access_token']
+    with open(path, 'w+') as f:
+        f.write(refresh_token)
+    return access_token
+def main():
+    fo = open(path, "r+")
+    refresh_token = fo.read()
+    fo.close()
+    global num1
+    localtime = time.asctime( time.localtime(time.time()) )
+    access_token=gettoken(refresh_token)
+    headers={
+    'Authorization':access_token,
+    'Content-Type':'application/json'
+    }
+    
+    random.shuffle(api_list)  # 执行洗牌算法，使得每次相邻调用的顺序不一致
+
+    try:
+        for api_url in api_list:
+            time.sleep(random.randrange(2, 15))
+            if req.get(api_url, headers=headers).status_code == 200:
+                print("调用成功: ", api_url)
+            else:
+                print("调用异常: ", api_url)
+
+        print('此次运行结束时间为: ', time.asctime(time.localtime(time.time())))
+    except:
+        print("调用出现异常，pass")
+        pass
+        
+for _ in range(random.randrange(2, 5)):
+    time.sleep(30 * random.randrange(1, 8))
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# MartialArtist
-MartialArtist
+# AutoApiSecret-加密版
+AutoApi系列：AutoApi、AutoApiSecret、AutoApiSR、AutoApiS
+
+# 置顶 #
+* 本项目是建立在[原教程](https://blog.432100.xyz/index.php/archives/50/)可以正确调用api的**假设**上的，核心是paran/黑幕大佬的py脚本。
+* 本项目只是提供一个自动、免费、无需额外设备的脚本运行方式，换句话说，**借用github的电脑/服务器来干活**。（因为原教程需要服务器/超长时间运转的设备，大部分人都不具备，本项目应运而生）
+* 本项目运行依赖**github action**服务，此功能github固有而**非私人提供**的服务器，且整个运行过程只涉及你与github。
+* 请务必先阅读理解[原教程](https://blog.432100.xyz/index.php/archives/50/)的**原理说明、设计理念**。
+* **不保证一定能续期！不保证一定能续期！不保证一定能续期**！或者说，**只是增大续订可能性**。过期前、后30天都可能续期！！！
+* 若理解并接受上述说明，请接着操作；**若否，请点击浏览器右上角 X 。**
+
+### 项目说明 ###
+* 利用github action实现**定时自动调用api**，保持E5开发活跃。
+* **免费，不需要额外设备/服务器**，部署完不用管啦。
+* 加密版，隐藏应用id+机密，保护账号安全。
+
+### 特别说明/Thanks ###
+* 原教程博主-黑幕（酷安id-Paran）：https://blog.432100.xyz/index.php/archives/50/
+* 普通版地址：https://github.com/wangziyingwen/AutoApi
+* 加密版地址（推荐）：https://github.com/wangziyingwen/AutoApiSecret
+* 模仿人为应用开发版（包含升级步骤）：https://github.com/wangziyingwen/AutoApiSR
+* 超级版地址： https://github.com/wangziyingwen/AutoApiS
+* **常见错误及解决办法/更新日志**：https://github.com/wangziyingwen/Autoapi-test
+* 网页获取refresh_token小工具（不建议使用）：https://github.com/wangziyingwen/GetAutoApiToken
+* 视频教程：（我操作很慢，自行倍速/快进）
+   * 在线/下载地址：https://kino-onemanager.herokuapp.com/Video/AutoApi%E6%95%99%E7%A8%8B.mp4?preview
+   * B站：https://www.bilibili.com/video/av95688306/
+   
+ 
+### 备注 ###
+#### 2021年9月6号 ####
+
